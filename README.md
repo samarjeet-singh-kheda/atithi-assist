73091bf12ad431706c65395410b6ff953ff3f226
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-09-17 23:03:26 +0530

    CHORE: initial commit, setup done

---
+++ README.md
@@ -0,0 +1,1 @@
+Landing Page & chatbot component of Atithi Assist, an AI-based Ticket Booking chatbot.
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-green-400">Hello world!</h1>
+  );
+}
